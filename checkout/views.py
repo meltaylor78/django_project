9a6641fad43c61ca4391fab66e9e59833b0c5610
meltaylor78 <meltaylor78@gmail.com>
2021-06-02 18:48:57 +0000

    added stripe elements to the project

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IxybJLRigk8tGyAcdkuNWFTwyq4KVPUaWQj0FdRmlb6Go1B1KOYqyq4o4ji1uNDAzHZkSrulKzoNErSkp9DU6WH001D7B3PP4',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
