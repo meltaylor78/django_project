909576c7d946978f268cbba15d861b3045096c53
meltaylor78 <meltaylor78@gmail.com>
2021-06-20 21:55:59 +0000

    updated django secret key to use OS var

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r_m73lo=5v^zb7g@&&cue%$6dv%h@=pt62f8+_cdntob5vredh'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['django-walkthrough-project.herokuapp.com', 'localhost']
 
